e71b5738a483a8cafcf69174d2675b324f54a226
kazuooooo <matsumotokazuya7@gmail.com>
2016-01-12 23:12:47 +0900

    show bet check odds table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
+  FruitsBox = Struct.new(:name, :amount)
+  FruitsBoxes = []
   def index
   end
   def try_home
   end
   def bet_check
+    @odds_data = BetCheckScraper.scrape_odds_data
   end
 end
--- app/util/bet_check_scraper.rb
+++ app/util/bet_check_scraper.rb
@@ -1,20 +1,19 @@
 require 'mechanize'
-require 'pry'
 
 # 最新のオッズ情報を取得して返す
-class BetCheckScraper
+module BetCheckScraper
 
   class Race < Struct.new(:title, :race_num, :horce_objs); end
   class Horce < Struct.new(:horce_num, :horce_name, :tansyo, :fukusyo_min, :fukusyo_max); end
 
-  def scrape
+  def scrape_odds_data
     # ぺージ取得
     agent = Mechanize.new
     # オッズぺージにアクセス
     agent.post('http://www.jra.go.jp/JRADB/accessO.html', {
                 "cname" => "pw15oli00/6D"
               })
-    # 中山にアクセス
+    # TODO: 各場所でもう一段階ネストさせる
     agent.post('http://www.jra.go.jp/JRADB/accessO.html', {
                 "cname" => "pw15orl10062016010320160110/B3"
               })
@@ -35,6 +34,7 @@ class BetCheckScraper
       # 戻る
       agent.back
     end
+    races
   end
 
   def scraping_data(page_body)
@@ -45,11 +45,11 @@ class BetCheckScraper
     horces = html.css('.ozTanfukuTableUma tr:not(:first-child)')
     horce_objs = horces.map do |horce|
                    Horce.new(
-                    horce.css('.umaban').inner_text,
+                    horce.css('.umaban').inner_text.to_i,
                     horce.css('.bamei').inner_text,
-                    horce.css('.oztan').inner_text.lstrip,
-                    horce.css('.fukuMin').inner_text.lstrip,
-                    horce.css('.fukuMax').inner_text.lstrip
+                    horce.css('.oztan').inner_text.lstrip.to_f,
+                    horce.css('.fukuMin').inner_text.lstrip.to_f,
+                    horce.css('.fukuMax').inner_text.lstrip.to_f
                    )
                  end
 
@@ -60,5 +60,5 @@ class BetCheckScraper
                  horce_objs
                )
   end
+  module_function :scrape_odds_data, :scraping_data
 end
-sample = BetCheckScraper.new.scrape
