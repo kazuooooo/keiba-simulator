5149a6cde615ceeb6211d909e94cda50aab74400
kazuooooo <matsumotokazuya7@gmail.com>
2016-02-28 20:04:00 +0900

    add column to keiba scraping

--- scraping_gem/lib/keiba_scraping.rb
+++ scraping_gem/lib/keiba_scraping.rb
@@ -15,10 +15,21 @@ end
 
 module KeibaScraping
   # Race 日付、場所、第何Rか
-  class Race < Struct.new(:date, :place, :race_num); end
+  class Race < Struct.new(:date, :place, :race_num, :race_name, :cource, :rotation, :distance, :weather, :cource_condition); end
   # horce 着順、枠番、馬番、人気、オッズ
   class Horce < Struct.new(:ranking, :frame_num, :horce_num, :horce_name, :popularity, :odds); end
-  year = 1996
+  if ARGV[0].include?("..")
+    inputs = ARGV[0].split("..").map(&:to_i)
+    years = Range.new(inputs[0], inputs[1])
+  else
+    years = [ARGV[0].to_i]
+  end
+
+  years.each do |year|
+    # fileの削除(要vcr_cassetesの削除)
+    if File.exists?("#{year}.csv")
+      File.delete "#{year}.csv", File.expand_path("./vcr/vcr_cassettes/2013.yml")
+    end
     csv_file = "#{year}.csv"
     # CSVヘッダー記載
     CSV.open(csv_file,"w") do |csv|
@@ -26,6 +37,12 @@ module KeibaScraping
         "日付",
         "場所",
         "R",
+        "レース名",
+        "コース",
+        "周り",
+        "距離",
+        "天候",
+        "馬場",
         "着順",
         "枠番",
         "馬番",
@@ -52,7 +69,7 @@ module KeibaScraping
           race_list_links = date_list_page.links.select do |link|
                               /.回.*日/ === link.text
                             end
-          race_list_pages = race_list_links.map{ |link| begin link.click rescue retry end}
+          race_list_pages = race_list_links.map{ |link| begin link.click rescue retry end }
           #binding.pry
           # (1) のページそれぞれに対して
           race_list_pages.each do |race_list_page|
@@ -67,27 +84,25 @@ module KeibaScraping
                 # Race情報を取得
                 title_text = html.css('#raceTitDay').text
                 title_text_array = title_text.gsub(" ","").split("|")
-                if title_text_array.nil?
-                  binding.pry
-                  puts "aaa"
-                end
-                if title_text_array[1].nil?
-                  binding.pry
-                  puts "aaa"
-                end
                 # 日付
                 date = title_text_array[0]
                 # 場所
                 place = title_text_array[1][2,2]
                 # 第何Rか
                 race_num = html.css('#raceNo').text.gsub(/[^0-9]/,"").to_i
-                race_obj = Race.new(date, place, race_num)
-
+                race_name = html.css('.fntB').text.delete("\n")
+                # その他の情報
+                race_title_meta = html.css('#raceTitMeta').text.split("|")[0]
+                cource = race_title_meta.slice(/芝|ダート/)
+                rotation = race_title_meta.slice(/右|左|直線/)
+                distance = race_title_meta.slice(/ .*m/).delete(" ").delete("m")
+                weather = html.css('#raceTitMeta img:first')[0].attributes["alt"].value
+                cource_condition = html.css('#raceTitMeta img')[1].attributes["alt"].value
+                race_obj = Race.new(date, place, race_num, race_name, cource, rotation, distance, weather, cource_condition)
                 # horce情報を取得
                 horces = html.css('#resultLs tr:not(:first-child)').css('tr:not(:last-child)')
                 @horce_objs = []
                 horces.each do |horce|
-                  #binding.pry
                   ranking = horce.at_css('td.txC:nth-of-type(1)').inner_text.gsub(/[\r\n]/,"").to_i
                   frame_num = horce.at_css('td.txC:nth-of-type(2)').inner_text.gsub(/[\r\n]/,"").to_i
                   horce_num = horce.at_css('td.txC:nth-of-type(3)').inner_text.gsub(/[\r\n]/,"").to_i
@@ -111,6 +126,12 @@ module KeibaScraping
                             race_obj.date,
                             race_obj.place,
                             race_obj.race_num,
+                            race_obj.race_name,
+                            race_obj.cource,
+                            race_obj.rotation,
+                            race_obj.distance,
+                            race_obj.weather,
+                            race_obj.cource_condition,
                             horce_obj.ranking,
                             horce_obj.frame_num,
                             horce_obj.horce_num,
@@ -124,4 +145,5 @@ module KeibaScraping
           end
       end
     end
+  end
 end
